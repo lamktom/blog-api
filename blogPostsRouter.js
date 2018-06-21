fb661b3299c35525cd5e2f83d31763ca463c9b5c
lamktom <lamktom@yahoo.com>
2018-06-20 22:27:20 -0700

    fixed bugs

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -18,7 +18,7 @@ BlogPosts.create('How to live a happier life', solution(), 'Tom Lam');
 // and return JSON objects of stored blog posts.
 // send back JSON representation of all blog posts
 // on GET requests to root
-router.get('/blog-posts', (req, res) => {
+router.get('/', (req, res) => {
 	res.json(BlogPosts.get()); 
 });
 
@@ -28,16 +28,18 @@ router.get('/blog-posts', (req, res) => {
 // the id, which `BlogPosts` will create. This endpoint should
 // send a 400 error if the post doesn't contain
 // `title`, `content`, and `author`
-router.post('/blog-posts', jsonParser, (req, res) => {
+router.post('/', jsonParser, (req, res) => {
 	const requiredFields = ['title', 'content', 'author'];
 	for (let i=0; i < requiredFields.length; i++) {
 		const field = requiredFields[i]; 
-		if(!(field in req.body)); {
+		if(!(field in req.body)) {
 			const message = `Missing "${field}" in request body`;
 			console.error(message); 
 			return res.status(400).send(message); 
 		}
 	}
+	const post = BlogPosts.create(req.body.title, req.body.content, req.body.author);
+	res.status(201).json(post); 
 }); 
 
 // add endpoint for PUT requests to update blogposts. it should
@@ -46,7 +48,7 @@ router.post('/blog-posts', jsonParser, (req, res) => {
 // the post matches the id of the path variable, and that the
 // following required fields are in request body: `id`, `title`,
 // `content`, `author`, `publishDate`
-router.delete('/blog-posts/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
 	BlogPosts.delete(req.params.id); 
 	console.log(`Deleted blog post "${req.params.id}"`);
 	res.status(204).end();  
@@ -55,7 +57,7 @@ router.delete('/blog-posts/:id', (req, res) => {
 // add endpoint for DELETE requests. These requests should
 // have an id as a URL path variable and call
 // `BlogPosts.delete()`
-router.put('/blog-posts/:id', jsonParser, (req, res) => {
+router.put('/:id', jsonParser, (req, res) => {
 	const requiredFields = ['id','title', 'content', 'author', 'publishDate'];
 	for (let i=0; i<requiredFields.length; i++) {
 		const field = requiredFields[i]; 
